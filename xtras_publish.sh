25d39d2c9a26daf98eef43e526dc0cf1402c6f8a
Dan Brough <dan@danbrough.org>
2022-10-02 16:25:19 +1300

    update

---
+++ xtras_publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+export PUBLISHING_URL="https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+
+[ -z "$SONATYPE_USER" ] && echo SONATYPE_USER not set && exit 1
+[ -z "$SONATYPE_PASSWORD" ] && echo SONATYPE_PASSWORD not set && exit 1
+[ -z "$SONATYPE_REPO_ID" ] && echo SONATYPE_REPO_ID not set && exit 1
+
+export PUBLISHING_USER="$SONATYPE_USER"
+export PUBLISHING_PASSWORD="$SONATYPE_PASSWORD"
+export REPOSITORY_ID="$SONATYPE_REPO_ID"
+
+if [ "$(uname)" = "Darwin" ]; then
+  #need JAVA_HOME to be set
+  export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-11.jdk/Contents/Home/
+  ./gradlew -PsignPublications `cat mac_targets.txt | sed -e '/^#/d'`
+  exit 0
+fi
+
+./gradlew -PsignPublications  publishAllPublicationsToSonaTypeRepository
+
