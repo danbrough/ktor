a6e1b7db67e3c60113d20a27f5fab296c1ced819
Dan Brough <dan@danbrough.org>
2022-10-07 13:48:27 +1300

    update

--- ktor-client/ktor-client-curl/build.gradle.kts
+++ ktor-client/ktor-client-curl/build.gradle.kts
@@ -1,4 +1,8 @@
 import org.jetbrains.kotlin.gradle.targets.native.tasks.*
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.konan.target.Family
+import org.jetbrains.kotlin.konan.target.KonanTarget
+
 
 apply<test.server.TestServerPlugin>()
 
@@ -59,5 +63,48 @@ kotlin {
         if (HOST_NAME != "windows") return@afterEvaluate
         val winTests = tasks.findByName("mingwX64Test") as? KotlinNativeTest? ?: return@afterEvaluate
         winTests.environment("PATH", WIN_LIBRARY_PATH)
+    }
+}
+
+
+
+
+
+val KonanTarget.platformName: String
+  get() {
+    if (family == Family.ANDROID) {
+      return when (this) {
+        KonanTarget.ANDROID_X64 -> "androidNativeX64"
+        KonanTarget.ANDROID_X86 -> "androidNativeX86"
+        KonanTarget.ANDROID_ARM64 -> "androidNativeArm64"
+        KonanTarget.ANDROID_ARM32 -> "androidNativeArm32"
+        else -> throw Error("Unhandled android target $this")
+      }
+    }
+    return name.split("_").joinToString("") { it.capitalize() }.decapitalize()
+  }
+
+val generateInteropsDefTaskName = "generateInteropsDef"
+
+tasks.register(generateInteropsDefTaskName) {
+    description = "Generate desktop/interop/libcurl.def from desktop/interop/libcurl_headers.h"
+    inputs.file("desktop/interop/libcurl_header.def")
+    outputs.file("desktop/interop/libcurl.def")
+    group = "interop"
+    doFirst {
+        val outputFile = outputs.files.files.first()
+        println("Generating $outputFile")
+        outputFile.printWriter().use { output ->
+            output.println(inputs.files.files.first().readText())
+            kotlin.targets.withType<KotlinNativeTarget>().forEach {
+                val konanTarget = it.konanTarget
+                output.println("compilerOpts.${konanTarget.name} = -Ibuild/kotlinxtras/curl/${konanTarget.platformName}/include/curl \\")
+                output.println("\t-I/usr/local/kotlinxtras/libs/curl/${konanTarget.platformName}/include/curl ")
+                output.println("linkerOpts.${konanTarget.name} = -Lbuild/kotlinxtras/curl/${konanTarget.platformName}/lib \\")
+                output.println("\t-L/usr/local/kotlinxtras/libs/curl/${konanTarget.platformName}/lib ")
+                output.println("libraryPaths.${konanTarget.name} = build/kotlinxtras/curl/${konanTarget.platformName}/lib \\")
+                output.println("\t/usr/local/kotlinxtras/libs/curl/${konanTarget.platformName}/lib ")
+            }
+        }
     }
 }
